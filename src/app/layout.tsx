8be1862bb7106ed2bc4131ba37d2ffa2296e0067
TomiNavel <hola@tominavel.com>
2025-08-27 00:58:08 +0200

    update package.json and layout.tsx to integrate Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/layout/Header";
 import "../styles/main.scss";
 import Footer from "@/components/layout/Footer";
 import CookiesBanner from "../components/CookiesBanner";
-import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: "Cuarto y Mita",
@@ -19,16 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          src="https://stats.tominavel.com/umami.js"
-          data-website-id="0785b251-7bf9-4338-bf60-8e1c1b9b1a32"
-          strategy="afterInteractive"
-        />
-      </head>
       <body className="overflow-x-hidden">
         <Header />
         {children}
+        <Analytics />
         <CookiesBanner />
         <Footer />
       </body>
